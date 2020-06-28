5cd8e8d69a3e6605869a933a746300f9155d2afb
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 10:46:11 -0500

    move js files to dedicated folder and update links

---
+++ JSS/backgroundColor.js
@@ -0,0 +1,11 @@
+function getTeal(){
+    document.body.style.backgroundColor = "rgb(124,244,213,96%)";
+}
+
+function getBrown(){
+    document.body.style.backgroundColor = "rgb(140,123,93)";
+}
+
+function getOrange(){
+    document.body.style.backgroundColor = "rgb(242,132,62,95%)";
+}
