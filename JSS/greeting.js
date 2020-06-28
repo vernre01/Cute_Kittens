ed9d5f0498a500721aef6ad465ead3d8d989c12a
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 17:29:44 -0500

    add revote button to change greeting and added additional validation

--- JSS/greeting.js
+++ JSS/greeting.js
@@ -1,12 +1,17 @@
-var catLover = prompt('Do you love cats?', 'Yes or No?');
-var greeting;
+function welcome() {
+    var catLover = prompt('Do you love cats?', 'Yes or No?');
+    var greeting;
 
-if(catLover == 'Yes' || catLover == 'yes' || catLover == 'YES') {
-    greeting = 'Welcome Cat Lovers!';
-} else if (catLover == 'No' || catLover == 'no' || catLover == 'NO') {
-    greeting = 'No worries! You\'ll be a Cat Lover soon!';
-} else {
-    greeting = 'Can\'t decide? Well, welcome anyway!';
-}
+    if(catLover == 'Yes' || catLover == 'yes' || catLover == 'YES') {
+        greeting = 'Welcome Cat Lovers!';
+        } else if (catLover == 'No' || catLover == 'no' || catLover == 'NO') {
+            greeting = 'No worries! You\'ll be a Cat Lover soon!';
+            } else if (catLover >= 0) {
+                greeting = 'A number? Perhaps you\'d like to try again?';
+                }
+                else {
+                    greeting = 'Um... Could you try again? Yes or No will do!';
+    }
 
 document.getElementById('greeting').innerHTML = greeting;
+}
