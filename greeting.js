86852fb2be556a28ca33ca80a3e12a50331d15f0
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 09:14:20 -0500

    update to add possible capitalization errors to answers

--- greeting.js
+++ greeting.js
@@ -1,9 +1,9 @@
 var catLover = prompt('Do you love cats? Yes or No?');
 var greeting;
 
-if(catLover == 'Yes') {
+if(catLover == 'Yes' || catLover == 'yes' || catLover == 'YES') {
     greeting = 'Welcome Cat Lovers!';
-} else if (catLover == 'No') {
+} else if (catLover == 'No' || catLover == 'no' || catLover == 'NO') {
     greeting = 'No worries! You\'ll be a Cat Lover soon!';
 } else {
     greeting = 'Can\'t decide? Well, welcome anyway!';
