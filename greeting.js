99434a378d512167aa4e9c8cee0e32194a15248a
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 10:37:18 -0500

    update greeting script to reference span

--- greeting.js
+++ greeting.js
@@ -1,12 +1,12 @@
 var catLover = prompt('Do you love cats?', 'Yes or No?');
-var greeting;
+var greeting = document.getElementById('greeting');
 
 if(catLover == 'Yes' || catLover == 'yes' || catLover == 'YES') {
-    greeting = 'Welcome Cat Lovers!';
+    greeting.textContent = 'Welcome Cat Lovers!';
 } else if (catLover == 'No' || catLover == 'no' || catLover == 'NO') {
-    greeting = 'No worries! You\'ll be a Cat Lover soon!';
+    greeting.textContent = 'No worries! You\'ll be a Cat Lover soon!';
 } else {
-    greeting = 'Can\'t decide? Well, welcome anyway!';
+    greeting.textContent = 'Can\'t decide? Well, welcome anyway!';
 }
 
-document.write('<h1>' + greeting + '</h1>');
+document.write(greeting);
