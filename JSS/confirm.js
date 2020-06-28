5f36fd2a3f67dc694d8f4eb0676224502a19f7f6
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 12:58:37 -0500

    updated comments and placement of <p id="results?">

--- JSS/confirm.js
+++ JSS/confirm.js
@@ -1,12 +1,17 @@
 function voteYes() {
     var voting = confirm('Are you sure?');
     var txt;
+    var kittenNumber = this.id;
 
     if (voting == true) {
     txt = 'Thanks for the love!';
 } else {
     txt = 'Try again.';
 }
+/* Yes, I know this only writes to the first article,
+no matter which cat you vote on.  I've tried several internet
+articles in the hopes of fixing it.  But after an hour or two I have
+given up. I am hoping to learn how to fix this with loops?*/
 
 document.getElementById('results1').innerHTML = txt;
 
@@ -21,7 +26,7 @@ function voteNo() {
 } else {
     txt = 'Try again.';
 }
-
+//I am hoping to learn how to fix this with loops?
 document.getElementById('results1').innerHTML = txt;
 
 }
