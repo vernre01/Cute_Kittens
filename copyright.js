1b757496840565398f884e9f5b14a4b85f3b778c
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 10:26:14 -0500

    remove duplicate greeting code

--- copyright.js
+++ copyright.js
@@ -1,14 +1,3 @@
 var year = document.getElementById('copyright');
 var today = new Date();
 year.textContent = today.getFullYear();
-
-var catLover = prompt('Do you love cats? Yes or No?');
-var greeting;
-
-if(catLover == 'Yes') {
-    greeting = 'Welcome Cat Lovers!';
-} else if (catLover == 'No') {
-    greeting = 'No worries! You\'ll be a Cat Lover soon!';
-} else {
-    greeting = 'Please enter Yes or No';
-}
