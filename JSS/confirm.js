b7c97552bbe28b826a3b0a9c5f77d8e2f4094c22
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 17:12:37 -0500

    fix function by adding kittenNumber, now writes at correct results ID

--- JSS/confirm.js
+++ JSS/confirm.js
@@ -1,32 +1,27 @@
-function voteYes() {
-    var voting = confirm('Are you sure?');
-    var txt;
-    var kittenNumber = this.id;
+var kittenNumber;
 
-    if (voting == true) {
-    txt = 'Thanks for the love!';
-} else {
-    txt = 'Try again.';
-}
-/* Yes, I know this only writes to the first article,
-no matter which cat you vote on.  I've tried several internet
-articles in the hopes of fixing it.  But after an hour or two I have
-given up. I am hoping to learn how to fix this with loops?*/
-
-document.getElementById('results1').innerHTML = txt;
-
-}
-
-function voteNo() {
+function voteYes(kittenNumber) {
     var voting = confirm('Are you sure?');
     var txt;
 
     if (voting == true) {
-    txt = 'Oh, well! Maybe the next one.';
-} else {
-    txt = 'Try again.';
-}
-//I am hoping to learn how to fix this with loops?
-document.getElementById('results1').innerHTML = txt;
+        txt = 'Thanks for the love!';
+        } else {
+            txt = 'Try again.';
+    }
 
+document.getElementById('results' + kittenNumber).innerHTML = txt;
+}
+
+function voteNo(kittenNumber) {
+    var voting = confirm('Are you sure?');
+    var txt;
+
+    if (voting == true) {
+        txt = 'Oh, well! Maybe the next one.';
+        } else {
+            txt = 'Try again.';
+    }
+
+document.getElementById('results' + kittenNumber).innerHTML = txt;
 }
