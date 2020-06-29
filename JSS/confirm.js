456acb88146c07781303d7f6f89e5d79325392c1
Rebecca Vernon <vernre01@gmail.com>
2020-06-28 19:53:26 -0500

    add basic vote counting code, count increases for all error

--- JSS/confirm.js
+++ JSS/confirm.js
@@ -1,4 +1,6 @@
 var kittenNumber;
+var y = document.getElementById('voteYes' + kittenNumber);
+var n = document.getElementById('voteNo' + kittenNumber);
 
 function voteYes(kittenNumber) {
     var voting = confirm('Are you sure?');
@@ -6,11 +8,15 @@ function voteYes(kittenNumber) {
 
     if (voting == true) {
         txt = 'Thanks for the love!';
+        if (y === undefined) {
+            y = 0;
+        }
+        y++;
         } else {
             txt = 'Try again.';
     }
-
-document.getElementById('results' + kittenNumber).innerHTML = txt;
+    document.getElementById('voteYes' + kittenNumber).innerHTML = ' ' + y;
+    document.getElementById('results' + kittenNumber).innerHTML = txt;
 }
 
 function voteNo(kittenNumber) {
@@ -19,9 +25,13 @@ function voteNo(kittenNumber) {
 
     if (voting == true) {
         txt = 'Oh, well! Maybe the next one.';
+        if (n === undefined) {
+            n = 0;
+        }
+        n++;
         } else {
             txt = 'Try again.';
     }
-
+document.getElementById('voteNo' + kittenNumber).innerHTML = ' ' + n;
 document.getElementById('results' + kittenNumber).innerHTML = txt;
 }
