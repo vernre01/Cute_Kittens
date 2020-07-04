3fb0cf85e1393e0ef4693f77fc9dae6d7b033914
Rebecca Vernon <vernre01@gmail.com>
2020-07-04 12:42:18 -0500

    update while loop to fix infinite loop

--- JSS/greeting.js
+++ JSS/greeting.js
@@ -4,17 +4,17 @@ function welcome() {
     var catLover = prompt('Do you love cats?', 'Yes or No?');
     var greeting;
 
-    while (catLover === null || (catLover.toLowerCase().trim() !== 'yes' && catLover.toLowerCase().trim() !== 'no')) {
-        prompt('Please enter Yes or No!');
-        if (catLover.toLowerCase().trim() === 'yes' || catLover.toLowerCase().trim() === 'no') {
+    while (catLover !== null) {
+        if (catLover.toLowerCase().trim() === 'yes') {
+            greeting = 'Welcome Cat Lovers!';
+            document.getElementById('greeting').innerHTML = greeting;
             break;
-        }
-    }
-
-    if(catLover.toLowerCase().trim() === 'yes') {
-        greeting = 'Welcome Cat Lovers!';
         } else if (catLover.toLowerCase().trim() === 'no') {
-            greeting = 'No worries! You\'ll be a Cat Lover soon!';   
+            greeting = 'No worries! You\'ll be a Cat Lover soon!';
+            document.getElementById('greeting').innerHTML = greeting; 
+            break;  
+        } else {
+            catLover = prompt('Please enter Yes or No');
         }
-document.getElementById('greeting').innerHTML = greeting;
     }
+}
