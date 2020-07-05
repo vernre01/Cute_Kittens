cf76ff4e8a603f8d2743e12ee22b4ec4f2937271
Rebecca Vernon <vernre01@gmail.com>
2020-07-05 11:17:22 -0500

    update for loop, still not working but am now actually getting error message

--- JSS/showKittens.js
+++ JSS/showKittens.js
@@ -6,21 +6,19 @@ function showKittens() {
     //Get # of kittens to show from user
     while (howMany !== null) {
         if (howMany >= 0 && howMany <= 9) {
-            return howMany;
+            //hide kittens above the number entered by user. "TypeError: Cannot read property 'style' of null at showKittens (showKittens.js:14) at HTMLButtonElement.<anonymous> (showKittens.js:1)"
+            for (var i = 0; i <= 9; i++) {
+                var thisCat = document.getElementById('article'+i);
+                
+                if (i < howMany) {
+                    thisCat.style.display = "inline-block";
+                } else {
+                    thisCat.style.display = "none";
+                }
+            }  
+            break;
         } else {
             howMany = prompt('Please enter a number between 1 and 9.');
         }
     }
-
-    //hide kittens above the number entered by user. DOESN'T WORK
-    for (var i = howMany + 1; i <= 9; i++) {
-        var thisCat = document.getElementById('article'+i);
-        var displaySetting = thisCat.style.display;
-        
-        if (displaySetting == 'inline-block') {
-            thisCat.style.display = "none";
-        } else {
-            thisCat.style.display = "inline-block";
-        }
-    }  
 }
